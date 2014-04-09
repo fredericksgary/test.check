4c75c3e23015717d9ee3b8138605bcfd719508f0
Gary Fredericks <fredericksgary@gmail.com>
2014-04-08 21:20:40 -0500

    Create generators/for macro
    This commit adds a generator macro that mimics the syntax of
    clojure.core/for and covers all of the functionality of bind, fmap,
    and such-that.

--- src/test/clojure/clojure/test/check/test.clj
+++ src/test/clojure/clojure/test/check/test.clj
@@ -356,6 +356,45 @@
   (prop/for-all [[element coll] vec-and-elem]
                 (some #{element} coll)))
 
+;; for works
+;; ---------------------------------------------------------------------------
+
+(def lists-and-counts
+  (gen/for [nums (gen/vector gen/nat)
+            :let [cardinality (count nums)]]
+    [nums cardinality]))
+
+(defspec for-works-correctly 100
+  (prop/for-all [[nums cardinality] lists-and-counts]
+    (= (count nums) cardinality)))
+
+(def lists-with-two-of-their-elements
+  (gen/for [nums  (gen/vector gen/nat)
+            :let  [cardinality (count nums)]
+            :when (> cardinality 1)
+            x     (gen/elements nums)
+            :let  [[befores [_x & afters]] (split-with #(not= % x) nums)
+                   nums-x (concat befores afters)]
+            y     (gen/elements nums-x)]
+    [nums x y]))
+
+(defspec complex-for-works-correctly 100
+  (prop/for-all [[nums x y] lists-with-two-of-their-elements]
+    (let [f (frequencies nums)]
+      ;; check that both x and y are in the list
+      (or (and (= x y) (> (f x) 1))
+          (and (not= x y) (pos? (f x)) (pos? (f y)))))))
+
+(def destructuring-usage
+  (gen/for [{:keys [foo]} (gen/hash-map :foo gen/nat)
+            :let [unused-binding 42]
+            vs (gen/vector gen/boolean foo)]
+    [foo vs]))
+
+(defspec for-destructuring-usage-spec 100
+  (prop/for-all [[n vs] destructuring-usage]
+    (= n (count vs))))
+
 ;; fmap is respected during shrinking
 ;; ---------------------------------------------------------------------------
 
