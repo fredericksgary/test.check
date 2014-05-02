c38002c740fb4f54fb1b6059554573d79632faa7
Gary Fredericks <garyf@groupon.com>
2014-05-02 16:26:06 +0000

    Don't need for in the fork anymore because I put it in test.chuck

--- src/test/clojure/clojure/test/check/test.clj
+++ src/test/clojure/clojure/test/check/test.clj
@@ -356,35 +356,6 @@
   (prop/for-all [[element coll] vec-and-elem]
                 (some #{element} coll)))
 
-;; for works
-;; ---------------------------------------------------------------------------
-
-(def lists-and-counts
-  (gen/for [nums (gen/vector gen/nat)
-            :let [cardinality (count nums)]]
-    [nums cardinality]))
-
-(defspec for-works-correctly 100
-  (prop/for-all [[nums cardinality] lists-and-counts]
-    (= (count nums) cardinality)))
-
-(def lists-with-two-of-their-elements
-  (gen/for [nums  (gen/vector gen/nat)
-            :let  [cardinality (count nums)]
-            :when (> cardinality 1)
-            x     (gen/elements nums)
-            :let  [[befores [_x & afters]] (split-with #(not= % x) nums)
-                   nums-x (concat befores afters)]
-            y     (gen/elements nums-x)]
-    [nums x y]))
-
-(defspec complex-for-works-correctly 100
-  (prop/for-all [[nums x y] lists-with-two-of-their-elements]
-    (let [f (frequencies nums)]
-      ;; check that both x and y are in the list
-      (or (and (= x y) (> (f x) 1))
-          (and (not= x y) (pos? (f x)) (pos? (f y)))))))
-
 ;; fmap is respected during shrinking
 ;; ---------------------------------------------------------------------------
 
