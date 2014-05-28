731229f4c3a11b8f5a0212ae06ef3cb9101a170e
Gary Fredericks <garyf@groupon.com>
2014-05-28 22:31:49 +0000

    Bump fork version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.gfredericks/test.check "0.5.8-p11"
+(defproject com.gfredericks/test.check "0.5.8-p12"
   :description "A QuickCheck inspired property-based testing library."
   :url "https://github.com/clojure/test.check"
   :license {:name "Eclipse Public License"
