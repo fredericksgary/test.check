3c5e2c6d1832a36e98e389775696ec54466cfac2
Gary Fredericks <garyf@groupon.com>
2014-05-28 17:40:06 +0000

    Update patch version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.gfredericks/test.check "0.5.8-p10"
+(defproject com.gfredericks/test.check "0.5.8-p11"
   :description "A QuickCheck inspired property-based testing library."
   :url "https://github.com/clojure/test.check"
   :license {:name "Eclipse Public License"
