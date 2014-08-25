53d018e8a839750daaf42a4f65c0a5efe138e979
Gary Fredericks <fredericksgary@gmail.com>
2014-08-25 09:47:07 -0500

    Release 0.5.9-p1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.gfredericks/test.check "0.5.9-p1-SNAPSHOT"
+(defproject com.gfredericks/test.check "0.5.9-p1"
   :description "A QuickCheck inspired property-based testing library."
   :url "https://github.com/clojure/test.check"
   :license {:name "Eclipse Public License"
