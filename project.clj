a21bf4395f6371001f5c0e2122e7933405eb7e01
Gary Fredericks <fredericksgary@gmail.com>
2014-11-05 11:12:05 -0600

    Set fork group/version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojure/test.check "0.5.9"
+(defproject com.gfredericks.forks.org.clojure/test.check "0.5.9-78b7cf8"
   :description "A QuickCheck inspired property-based testing library."
   :url "https://github.com/clojure/test.check"
   :license {:name "Eclipse Public License"
@@ -9,4 +9,5 @@
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]}}
   :global-vars {*warn-on-reflection* true}
   :plugins [[codox "0.8.10"]]
+  :deploy-repositories [["releases" :clojars]]
   :codox {:defaults {:doc/format :markdown}})
