c8086c9ed5a492fd1fd1607e82402ab3d28ceb88
Gary Fredericks <fredericksgary@gmail.com>
2014-12-06 14:02:13 -0600

    Version 0.6.1-p2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.gfredericks.forks.org.clojure/test.check "0.6.1-p1"
+(defproject com.gfredericks.forks.org.clojure/test.check "0.6.1-p2-SNAPSHOT"
   :description "A QuickCheck inspired property-based testing library."
   :url "https://github.com/clojure/test.check"
   :license {:name "Eclipse Public License"
